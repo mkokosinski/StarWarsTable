8964cef33f5ec1bf46fda59e7d7f109fe4e5c69a
Michał Kokosiński <19owen91@gmail.com>
2021-09-28 21:55:56 +0200

    outside detect click

---
+++ src/hooks/useDetectOutsideClick.js
@@ -0,0 +1,26 @@
+import { useCallback, useEffect } from 'react'
+
+const useDetectOutsideClick = (ref, handler, onlyParentCanClose = false) => {
+  const detectExit = useCallback(
+    (e) => {
+      if (!ref.current || ref.current.contains(e.target)) {
+        return
+      }
+      handler()
+    },
+    [handler, ref],
+  )
+
+  useEffect(() => {
+    if (ref.current) {
+      const node = onlyParentCanClose ? ref.current.parentNode : document
+
+      node.addEventListener('mousedown', detectExit)
+      return () => {
+        node.removeEventListener('mousedown', detectExit)
+      }
+    }
+  }, [detectExit, onlyParentCanClose, ref])
+}
+
+export default useDetectOutsideClick
